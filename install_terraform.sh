b8b71a84d9613cff1aba3bb6e58e5fa2a8cd8904
Ubuntu <ubuntu@ip-172-31-38-211.ap-south-1.compute.internal>
2023-05-06 09:13:12 +0000

    add this script to repo

---
+++ install_terraform.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Terraform
+TERRAFORM_VERSION="0.15.5"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+
+# Install unzip (if not already installed)
+sudo apt-get update
+sudo apt-get install -y unzip
+
+# Download and extract Terraform
+curl -LO $TERRAFORM_URL
+unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+
+# Move Terraform binary to a directory in the system's PATH
+sudo mv terraform /usr/local/bin/
+
+# Verify the installation
+terraform version
+
